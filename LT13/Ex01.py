762109dcf0ac7e9f1e8833edc32c352cd7d95f22
kenz-vn <kenz21042007@gmail.com>
2026-03-30 15:31:46 +0700

    Add files via upload

---
+++ LT13/Ex01.py
@@ -0,0 +1,6 @@
+import math
+n = float(input("nhập số: "))
+if  n < 0:
+    print("Lỗi: không thể tính căn bậc 2 của số âm")
+else:
+    print('Căn bậc 2 là: ',math.sqrt(n))
---
+++ LT13/Ex03.py
@@ -0,0 +1,8 @@
+tong = 0
+print("Các số chẵn từ 1 đến 20:")
+
+for i in range(2, 21, 2):
+    print(i, end=" ")
+    tong += i
+
+print("\nTổng:", tong)
